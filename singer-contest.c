dbd1610b667a38c9c86bdbb70ab5920ebf281df0
79laowang <edward.wang@163.com>
2019-10-17 23:13:26 -0400

    singer contest counting scores

---
+++ singer-contest.c
@@ -0,0 +1,29 @@
+/*------------------------------------------------------------------------------
+* File Name:   singer-contest.c
+* Purpose:     4. 歌星大赛 
+*
+* Author:      Ke Wang
+*
+* Created:     2019-10-17
+* Copyright:   (c) Ke Wang 2019
+* Licence:     <your licence>
+*-----------------------------------------------------------------------------*/
+
+#include <stdio.h>
+int main()
+{
+	int integer, i, max, min, sum;
+	max=-32768;
+	min=32768;
+	sum=0;
+	for (i=1; i<=10; i++){
+        printf("Input number %d=",i);
+        scanf("%d",&integer);
+		sum+=integer;
+		if (integer > max) max = integer;
+		if (integer < min) min = integer;
+	}
+	printf("Canceled max score: %d\nCanceled min score: %d\n", max, min);
+	printf("Average score: %d\n",(sum - max - min)/8);
+    return (0);
+}
