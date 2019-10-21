3409c08505c9216092daa11ae16eeba43f7ef28e
Ke <edward.wang@163.com>
2019-10-21 14:19:47 +0800

    add comments

--- get-max-divisor.c
+++ get-max-divisor.c
@@ -1,12 +1,26 @@
-main()
+/*------------------------------------------------------------------------------
+* File Name:   get-max-divisor.c
+* Purpose:     5. 求最大约数
+*
+* Author:      Ke Wang
+*
+* Created:     2019-10-17
+* Copyright:   (c) Ke Wang 2019
+* Licence:     <your licence>
+*-----------------------------------------------------------------------------*/
+
+#include <stdio.h>
+
+int main()
 {
     long i;
     int j;
-    printf("Please input number:");
+    printf("Please input number >100:");
     scanf("%ld",&i);
     for(j=999;j>=100;j--)
         if(i%j==0){
             printf("The max factor with 3 digits in %ld is:%d.\n",i,j);
             break;
         }
+    return (0);
 }
