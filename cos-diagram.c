6bdad039beffc154b6ef42d17c16b190ac28fa73
Ke <edward.wang@163.com>
2019-10-27 11:43:52 +0800

    add comments

--- cos-diagram.c
+++ cos-diagram.c
@@ -12,18 +12,20 @@
 #include <stdio.h>
 #include <math.h>
 
+#define WIDTH 62
+
  /* Compile with  gcc -o cos-diagram cos-diagram.c -lm */
 int main()
 {
-    double y;
-    int x, m;
-    for (y=1;y>=-1;y-=0.1)
+    double y;   /* 列 */
+    int x, m;   /* x为行 */
+    for (y=1;y>=-1;y-=0.1)   /* y为列方向，值从1到-1，步长0.1 */
     {
-        m=acos(y)*10;
+        m=acos(y)*10;    /*计算出y对应的弧度m,乘10为图形放大倍数 */
         for (x=1;x<m;x++) printf(" ");
-        printf("*");
-        for(;x<62-m;x++) printf(" ");
-        printf("*\n");
+        printf("*");   /* 控制打印左侧的*号 */
+        for(;x<WIDTH-m;x++) printf(" ");
+        printf("*\n"); /* 控制打印同一行对应右侧的*号 */
     }
     return (0);
 }
