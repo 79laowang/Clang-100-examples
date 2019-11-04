9c9be59f50df53f270c0e48d286ccd28966063d8
79laowang <edward.wang@163.com>
2019-11-04 10:50:19 +0800

    Add comments

--- zero-of-100fact.c
+++ zero-of-100fact.c
@@ -12,10 +12,10 @@
 #include <stdio.h>
 int main()
 {
-    int a, count=0;
-    for(a=5; a<=100;a+=5){
-        count ++;
-        if (!(a%25)) count++;
+    int a, count = 0;  /* 循环从5开始，以5的倍数为步长，试探整数 */
+    for (a = 5; a <= 100; a += 5){
+        count ++;   /* 若为5的倍数，计数器加1 */
+        if (!(a % 25)) count++; /* 25倍数，计数器再加1 */
     }
     printf("The number of 0 in the end of 100! is: %d.\n",count);
     return (0);
