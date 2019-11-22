868ce46c061ff8fe91c5aab925f7cec092bf6e97
79laowang <edward.wang@163.com>
2019-11-22 14:33:52 +0800

    for statement missed one loop when equal to 1898, and change the year to 2019

--- sum_1898_primes34.c
+++ sum_1898_primes34.c
@@ -15,27 +15,30 @@
 #define NUM 320
 
 int number[NUM];
-int fflag(int i)
+
+int is_prime(int i)
 {
     int j;
     if (i <= 1) return (0);
-    if (i == 2) return (1);
-    if (! (i % 2)) return (0);
-    for (j = 3; j <= (int)(sqrt((double)i) + 1); j += 2) {
-        if (! (i % j)) return (0);
-    }
+    if (i <= 2) return (1);
+    if (!(i % 2)) return (0);
+    for (j = 3; j <= (int)(sqrt((double)i) + 1); j += 2)
+        if(!(i % j)) return (0);                                                                                                                                                             
     return (1);
 }
 
 int main()
 {
-    int i, j, count = 0;
+    int i, j = 0, count = 0;
+    int year = 2019;
     printf("There are following primes sequences in first row:\n");
-    for (j = 0, i = 3; i <= 1993; i += 2) {
-        if (fflag(i)) number[j++] = i;
+    for (i = 3; i <= year; i += 2){
+        if (is_prime(i)) {
+            number[j++] = i;
+        }
     }
-    for ( j--; number[j] > 1898; j--) {
-        for (i = 0; number[j] - number[i] > 1898; i++) {
+    for (j--; number[j] > 1898; j--) {
+        for (i = 0; (number[j] - number[i]) >= 1898; i++) {
             if (number[j] - number[i] == 1898) 
                 printf(" (%d). %3d,......,%d\n", ++count, number[i], number[j]);
         }
